7b73097e74442442342309a8d930342b5a0baa7a
spinellimichele91 <43318050+spinellimichele91@users.noreply.github.com>
2018-09-16 22:07:25 +0200

    Update ex 2-5.c

--- chapter 02/ex 2-5.c
+++ chapter 02/ex 2-5.c
@@ -1,9 +1,9 @@
-/* Write a program that asks the user to enter a value for x and then display the vvalue
-of the following polynomial: 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6  */
+/************************************************************************************ 
+Write a program that asks the user to enter a value for x and then display the value
+of the following polynomial: 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6  
+*************************************************************************************/
 
 #include <stdio.h>
-
-
 
 int main()
 {
@@ -18,7 +18,6 @@ int main()
     int equation = (3 * xTimesfive) + (2 * xTimesfour) -
     (5 * xTimesthree) - (xTimestwo) +(7 * x) - (6);
     printf("Result of the equation 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6: %d\n\n", equation);
-
 
     return(0);
 }
