df09045929e696b24e6e1bc09b21f32a4b12d1a6
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-17 10:40:21 +0200

    Update ex 3-5.c

--- chapter 03/ex 3-5.c
+++ chapter 03/ex 3-5.c
@@ -1,5 +1,7 @@
-/* Write a program that asks the user to enter the numbers from 1 to 16 (in any order) and then displays
-the numbers in a 4 by 4 arrangement, followed by the sums of the rows, columns, and diagonals. */
+/***************************************************************************************************** 
+Write a program that asks the user to enter the numbers from 1 to 16 (in any order) and then displays
+the numbers in a 4 by 4 arrangement, followed by the sums of the rows, columns, and diagonals. 
+******************************************************************************************************/
 
 #include <stdio.h>
 
@@ -7,6 +9,7 @@ int main()
 {
     int one, two, three, four, five, six, seven, eight, nine,
     ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen;
+    
     printf("Enter the numbers from 1 to 16 in any order: ");
     scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
           &one, &two, &three, &four, &five, &six, &seven, &eight, &nine,
@@ -29,10 +32,6 @@ int main()
 
     printf("Diagonal sums: %d %d\n", one + six + eleven + sixteen,
                                 four + seven + ten + thirteen);
-
-    //16 3 2 13 5 10 11 8 9 6 7 12 4 15 14 1
-    //16 1 15 2 14 3 13 4 12 5 11 6 10 7 9 8
-
 
     return 0;
 }
