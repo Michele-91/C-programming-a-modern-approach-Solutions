256c6591cfd76fb6e06d0f8bb6be23685156efc0
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-18 12:51:16 +0200

    Update and rename ex 4-5.c to 05.c

--- chapter 04/projects/05.c
+++ chapter 04/projects/05.c
@@ -1,5 +1,7 @@
-/* Rewrite the upc.c program of Section 4.1 so that the user enter 11 digits at one time,
- * instead of entering one digit, then five digits, and then another five digits   */
+/***************************************************************************************
+Rewrite the upc.c program of Section 4.1 so that the user enters 11 digits at one time,
+instead of entering one digit, then five digits, and then another five digits   
+***************************************************************************************/
 
 #include <stdio.h>
 
@@ -12,7 +14,7 @@ int main()
           &i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11);
 
     //first_sum = i1 + i3 + i5 + i7 + i9 + i11;
-    //second_sum = i2 + i4 + i6 + i8 + i10;             //ALTRO MODO
+    //second_sum = i2 + i4 + i6 + i8 + i10;             //OTHER METHOD
     //total = 3 * first_sum + second_sum;
     total = 3 * (i1 + i3 + i5 + i7 + i9 + i11) + (i2 + i4 + i6 + i8 + i10);
 
