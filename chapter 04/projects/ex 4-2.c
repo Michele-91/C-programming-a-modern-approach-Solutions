fc70c7bf37704fe21aba0ba8454f90e5e8aa41f8
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-18 12:42:16 +0200

    Update ex 4-2.c

--- chapter 04/projects/ex 4-2.c
+++ chapter 04/projects/ex 4-2.c
@@ -1,4 +1,6 @@
-/* Extend the program in Programming Project 1 (chapter 4) to handle three-digit numbers  */
+/*************************************************************************************
+Extend the program in Programming Project 1 (chapter 4) to handle three-digit numbers 
+**************************************************************************************/
 
 #include <stdio.h>
 
@@ -11,16 +13,16 @@ int main()
     digit_one = number / 100;
     //printf("digit_one: %d\n", digit_one);
     number_remainder = number % 100;
-    digit_two = number_remainder / 10;                      //PRIMO MODO
+    digit_two = number_remainder / 10;                      //1ST METHOD (with alternative printf in comments)
     //printf("digit_two = %d\n", digit_two);
     digit_three = number_remainder % 10;
     //printf("digit_three = %d\n", digit_three);
 
     /*digit_one = number / 100;
-    digit_two = (number / 10) % 10;   // SECONDO MODO
+    digit_two = (number / 10) % 10;   // 2ND METHOD
     digit_three = number % 10;*/
 
-    /*printf("The reversal is %1d%1d%1d\n",                             //TERZO MODO
+    /*printf("The reversal is %1d%1d%1d\n",                             //3RD METHOD
            number % 10, (number / 10) % 10, number / 100);*/
 
     printf("\tThe reversal is %1d%1d%.1d\n", digit_three, digit_two, digit_one);
