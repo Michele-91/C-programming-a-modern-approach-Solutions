bb8414726331effbb7d4c92170b0d607bf96e3d7
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-23 15:11:24 +0200

    Update 07.c

--- chapter 06/projects/07.c
+++ chapter 06/projects/07.c
@@ -1,5 +1,7 @@
-/* Write a program that prompts the user to enter a number n, then prints all even squares between
- * 1 and n */
+/********************************************************************************
+Write a program that prompts the user to enter a number n, then prints all even 
+squares between 1 and n 
+*********************************************************************************/
 
 #include <stdio.h>
 
