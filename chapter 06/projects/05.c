8df331831189eac020783c66e564b94b342f6482
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-23 15:09:12 +0200

    Update 05.c

--- chapter 06/projects/05.c
+++ chapter 06/projects/05.c
@@ -1,6 +1,8 @@
-/* Programming Project 1 in Chapter 4 asked you to write a program that displays a two-digit number
- * with its digits reversed. Generalize the program so that the number can have one, two, three, or
- * more digits. Hint: Use a do loop that repeatedly divides the number by 10, stopping when it reaches 0 */
+/******************************************************************************************************
+Programming Project 1 in Chapter 4 asked you to write a program that displays a two-digit number
+with its digits reversed. Generalize the program so that the number can have one, two, three, or
+more digits. Hint: Use a do loop that repeatedly divides the number by 10, stopping when it reaches 0 
+*******************************************************************************************************/
 
 #include <stdio.h>
 
