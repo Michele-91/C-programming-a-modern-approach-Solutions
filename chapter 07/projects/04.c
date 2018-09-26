f02047e6278e1890d698b872aa5b8f724d4e11fa
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-26 18:00:39 +0200

    Update and rename ex 7-4.c to 04.c

--- chapter 07/projects/04.c
+++ chapter 07/projects/04.c
@@ -1,3 +1,13 @@
+/***************************************************************************
+Write a program that translates an alphabetic phone number into numeric
+form.
+
+(In case you don't have a telephone nearby, here are the letters on the
+2=ABC, 3=DEF, 4=GHI, 5=JKL, 6=MNO, 7=PRS, 8=TUV, 9=WXY.) If the original
+phone number contains nonalphanumeric characters (digits or punctuation,
+for example), leave them unchanged.
+You may assume that any letters entered by the user are upper case.
+***************************************************************************/
 
 #include <stdio.h>
 #include <math.h>
