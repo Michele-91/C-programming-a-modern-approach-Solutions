9f517dfcf63dd6de0cb608deafc5d0f68e39e741
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-26 18:02:58 +0200

    Update ex 7-6.c

--- chapter 07/projects/ex 7-6.c
+++ chapter 07/projects/ex 7-6.c
@@ -1,3 +1,7 @@
+/************************************************************************
+Write a program that prints the values of sizeof(int), sizeof(short),
+sizeof(long), sizeof(float), sizeof(double) and sizeof(long double).
+************************************************************************/
 
 #include <stdio.h>
 #include <math.h>
@@ -11,10 +15,6 @@ int main()
     printf("%d\n", (int) sizeof(float));
     printf("%d\n", (int) sizeof(double));
     printf("%d\n", (int) sizeof(long double));
-
-
-
-
 
     return 0;
 }
