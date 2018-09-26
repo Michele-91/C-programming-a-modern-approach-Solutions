adcc801193a5d78f8660c478d402a192e1f5ef07
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-26 18:30:21 +0200

    Update ex 7-14.c

--- chapter 07/projects/ex 7-14.c
+++ chapter 07/projects/ex 7-14.c
@@ -1,30 +1,54 @@
+/********************************************************************************* 
+Write a program that uses Newton's method to compute the square root of
+a positive floating-point number:
+
+    Enter a positive number: 3
+    Square root: 1.73205
+    
+Let x be the number entered by the user. Newton's method requires an initial
+guess y for the square root of x (we'll use y = 1). Successive guesses are
+found by computing the average of y and x/y.
+                                  
+                              Average of
+x     y             x/y       y and x/y
+-------------------------------------------
+3   1            3            2
+3   2            1.5          1.75
+3   1.75         1.71429      1.73214
+3   1.73214      1.73196      1.73205
+3   1.73205      1.73205      1.73205
+
+Note that the values of y get progressively closer to the true square root
+of x. For greater accuracy, your program should use variables of type double
+rather than float. Have the program terminate when the absolute value of the
+difference between the old value of y and the new of y is less than the
+product of .00001 and y. Hint: Call the fabs function to find the absolute
+value of a double. (You'll need to include the <math.h> header at the
+beginning of your program in order to use fabs.)
+**********************************************************************************/
 
 #include <stdio.h>
 #include <ctype.h>
 
-
-int main() {
-
-
-    char c;
+int main() 
+{
+    char ch;
     float len;
     int words;
     len = 0.0f;
     words = 1;
 
     printf("Enter a sentence: ");
-    while((c = getchar()) != '\n')
+    while((ch = getchar()) != '\n')
     {
-        if(c == ' ')
+        if(ch == ' ')
         {
             words++;
         } else
         {
             len++;
         }
-
     }
-
     printf("Average word length: %.1f\n", len / words);
 
     return 0;
