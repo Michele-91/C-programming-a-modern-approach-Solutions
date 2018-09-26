729b1345eabdd227503cc53534407f8156d3b6e1
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-26 18:01:38 +0200

    Update and rename ex 7-5.c to 05.c

--- chapter 07/projects/05.c
+++ chapter 07/projects/05.c
@@ -1,3 +1,14 @@
+/************************************************************************
+In the SCRABBLE Crossord Game, players form words using small tiles,
+each containing a letter and a face value. The face value varies from
+one letter to another, based on the letter's rarity. (Here are the
+face values: 1: AEILNORSTU, 2: DG, 3: BCMP, 4: FHVWY, 5: K, 8: JX,
+10: QZ.) Write a program that computes the value of a word by summing
+the values of its letters.
+Your program should allow any mixture of lower-case and upper-case
+letters in the word. Hint: Use the toupper library function.
+*************************************************************************/
+
 
 #include <stdio.h>
 #include <math.h>
