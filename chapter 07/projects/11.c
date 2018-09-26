7fca2d7b976401b89d2eb64664083c27c66f6f9f
AmmagEtaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-09-26 18:17:33 +0200

    Update and rename ex 7-11.c to 11.c

--- chapter 07/projects/11.c
+++ chapter 07/projects/11.c
@@ -1,3 +1,14 @@
+/*********************************************************************
+Write a program that takes the first name and last name entered by
+the user and displays the last name, a comma, and the first
+initial, followed by a period:
+ 
+Enter a first and last name: Lloyd Fosdick
+Fosdick, L.
+ 
+Ths user's input may contain extra spaces before the first name,
+between the first and last names, and after the last name.
+*********************************************************************/
 
 #include <stdio.h>
 #include <ctype.h>
