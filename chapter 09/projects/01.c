4a0f9aa78592f065623766a8d022ebfc23eabd36
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-08 02:00:19 +0200

    Update and rename ex9-1.c to 01.c

--- chapter 09/projects/01.c
+++ chapter 09/projects/01.c
@@ -1,3 +1,14 @@
+/**********************************************************************
+ * Write a program that asks the user to enter a series of integers
+ * (which it stores in an array), then sorts the integers by calling
+ * the function selection_sort. When given an array with n elements, 
+ * selection_sort must do the following: 
+ * 1. Search the array to find the largest element, then move it to 
+ *    the last position in the array.
+ * 2. Call itself recursively to sort the first n-1 elements of the 
+ *    array
+ **********************************************************************/
+
 #include <stdio.h>
 
 void selection_sort(int a[], int n);
