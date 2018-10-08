cd8f6f78c1a44d3c4e8e5309a542805378d20401
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-08 02:20:26 +0200

    Update and rename ex9-2.c to 02.c

--- chapter 09/projects/02.c
+++ chapter 09/projects/02.c
@@ -2,7 +2,7 @@
 Modify Programming Project 5 from Chapter 5 so that it uses a function
 to compute the amount of income tax. When passed an amount of taxable
 income, the function will return the tax due.
-***********************************************************************/
+************************************************************************/
 
 #include <stdio.h>
 
