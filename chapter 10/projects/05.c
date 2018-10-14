5781d5c4d3621edef59dcf5c46045eac393db0f0
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-14 07:36:35 +0200

    Update and rename ex10-5.c to 05.c

--- chapter 10/projects/05.c
+++ chapter 10/projects/05.c
@@ -3,8 +3,7 @@ Modify the poker.c program of Section 10.5 by allowing "ace-low" straights
 (ace, two, three, four, five).
 *****************************************************************************/
 
-
-#include <stdbool.h>    /* C99 only */
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
