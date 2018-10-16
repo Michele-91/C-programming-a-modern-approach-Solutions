aefba22d4805850ef2ae0c5763e63e2d875888aa
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-16 14:04:57 +0200

    Update 04.c

--- chapter 11 - Pointers/exercises/04.c
+++ chapter 11 - Pointers/exercises/04.c
@@ -2,7 +2,7 @@
 Write the following function: void swap(int *p, int *q);
 When passed the addresses of two variables, swap should exchange the 
 values of the variables:
-swap(&i, &j); /* exchanges the values of i and j */
+swap(&i, &j);
 ***********************************************************************/
 
 #include <stdio.h>
