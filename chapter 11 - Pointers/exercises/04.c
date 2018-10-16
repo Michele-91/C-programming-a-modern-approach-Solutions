97135507c322e65cf4f4b270bdc8eb7a3725a3dc
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-16 13:58:34 +0200

    Create 04.c

---
+++ chapter 11 - Pointers/exercises/04.c
@@ -0,0 +1,33 @@
+/**********************************************************************
+Write the following function: void swap(int *p, int *q);
+When passed the addresses of two variables, swap should exchange the values
+of the variables:
+swap(&i, &j); /* exchanges the values of i and j */
+***********************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 5
+
+void swap(int *p, int *q);
+
+int main(void)
+{
+    int i, j;
+
+    printf("\nEnter two integers: ");
+    scanf(" %d %d", &i, &j);
+
+    swap(&i, &j);
+    printf("Swapped numbers: %d %d\n", j, i);
+}
+
+void swap(int *p, int *q)
+{
+    int temp;
+
+    temp = *q;
+    *q = *p;
+    *p = temp;
+}
