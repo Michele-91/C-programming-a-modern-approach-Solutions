cd1717c47205cf869a64cfbed9ee78f5da050d03
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-19 10:01:57 +0200

    Create 07.c

---
+++ chapter 12 - Pointers and Arrays/exercises/07.c
@@ -0,0 +1,42 @@
+/***************************************************************************
+Write the following function:
+bool search(const int a[], int n, int key);
+
+a is an array to be searched, n is the number of elements in the array, 
+and key is the search key. search should return true if key matches some 
+element of a, and false if it doesn't. Use pointer arithmetic--not 
+subscripting--to visit array elements.
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 5
+
+bool search(const int a[], int n, int key);
+
+int main(void)
+{
+    int a[N], *p, key = 8;
+
+    printf("Enter %d numbers: ", N);
+    for(p = a; p < a + N; p++)
+        scanf(" %d", p);
+    
+    if(search(a, N, key))
+        printf("Key found; access granted\n");
+    else
+        printf("Key not found; access denied\n");
+
+}
+
+bool search(const int a[], int n, int key)
+{
+    const int *p;
+
+    for(p = a; p < a + n; p++)
+        if(*p == key)
+            return true;
+
+    return false;
+}
