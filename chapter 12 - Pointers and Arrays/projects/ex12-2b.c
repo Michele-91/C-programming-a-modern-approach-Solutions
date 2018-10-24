b0d9ca7e77a60ad270f3cf30a6907ff26346bacd
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-24 15:42:36 +0200

    Update ex12-2b.c

--- chapter 12 - Pointers and Arrays/projects/ex12-2b.c
+++ chapter 12 - Pointers and Arrays/projects/ex12-2b.c
@@ -1,19 +1,4 @@
 /***************************************************************************
-(a) Write a program that reads a message, then prints the reversal of the
-    message:
-
-Enter a message: Don't get mad, get even.
-Reversal is: .neve teg ,dam teg t'noD
-
-Hint: Read the message one character at a time (using getchar) and store the
-characters in an array. Stop reading when the array is full or the character
-read is '\n'.
-
-(b) Revise the program to use a pointer instead of an integer to keep track
-    of the current position in the array.
-***************************************************************************/
-
-/***************************************************************************
 (a) Write a program that reads a message, then checks whether it's a
 palindrome (the letters in the message are the same from right to left):
 
