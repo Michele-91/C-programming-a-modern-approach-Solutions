5a2af4e0452ecd09515f2f720038a4b77f9630b4
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-24 10:14:26 +0200

    Add files via upload

---
+++ chapter 12 - Pointers and Arrays/projects/ex12-1a.c
@@ -0,0 +1,46 @@
+/***************************************************************************
+(a) Write a program that reads a message, then prints the reversal of the
+    message:
+
+Enter a message: Don't get mad, get even.
+Reversal is: .neve teg ,dam teg t'noD
+
+Hint: Read the message one character at a time (using getchar) and store the
+characters in an array. Stop reading when the array is full or the character
+read is '\n'.
+
+(b) Revise the program to use a pointer instead of an integer to keep track
+    of the current position in the array.
+***************************************************************************/
+
+#include <stdio.h>
+
+#define LEN 30
+
+
+
+int main(void)
+{
+    char message[LEN], ch;
+    int i;
+    printf("Enter a message: ");
+    for(i = 0; i < LEN; i++)
+    {
+        if((ch = getchar()) == '\n') break;
+        message[i] = ch; 
+    }
+
+    printf("Reversal is: ");
+    for(i = LEN - 1; i >= 0; i--)
+        putchar(message[i]);
+    printf("\n");
+    
+    return 0;
+}
+
+
+/*int evaluate_position(char *board, int n)
+{
+
+    return 0;
+}*/
