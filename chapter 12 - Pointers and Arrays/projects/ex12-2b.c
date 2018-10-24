857ee7ea328d992706c9a5c243e6f433965e82b1
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-24 16:14:26 +0200

    Add files via upload

---
+++ chapter 12 - Pointers and Arrays/projects/ex12-2b.c
@@ -0,0 +1,71 @@
+/***************************************************************************
+(a) Write a program that reads a message, then checks whether it's a
+palindrome (the letters in the message are the same from right to left):
+
+Enter a message: He lived as a devil, eh?
+Palindrome
+
+Enter a message: Madam, I am Adam.
+Not a palindrome
+
+Ignore all characters that aren't letters. Use integer variables to
+keep track of positions in the array.
+
+(b) Revise the program to use pointers instead of integers to keep
+track of positions in the array.
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define LEN 30
+
+int read_message(char a[], int n);
+bool is_palindrome(char a[], int n);
+
+int main(void)
+{
+    char message[LEN];
+
+    int length = read_message(message, LEN);
+
+    if(is_palindrome(message, length))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+    printf("\n");
+
+    return 0;
+}
+
+int read_message(char a[], int n)
+{
+    int length = 0;
+    char ch, *p;
+    printf("\nEnter a message: ");
+    for(p = a; p < a + LEN; p++)
+    {
+        if((ch = getchar()) == '\n') break;
+        if(isalpha(ch) && ch != isblank(ch) && ch != ispunct(ch));
+        {
+            *p = tolower(ch);
+            length++;
+        }
+    }
+
+    return length;
+}
+
+bool is_palindrome(char a[], int n)
+{
+    char *p, *p2;
+    for(p = a, p2 = a + n - 1; 
+        p < a + n - 1; 
+        p++, p2--)
+    {
+            if(*p != *p2)
+                return false;
+    }
+    return true;
+}
