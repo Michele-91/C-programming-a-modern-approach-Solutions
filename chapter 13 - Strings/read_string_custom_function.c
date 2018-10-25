5cf2cb0a930e346751bc48280ffdb25930f8988a
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-25 14:22:40 +0200

    Update and rename 00.c to read_string_custom_function.c

---
+++ chapter 13 - Strings/read_string_custom_function.c
@@ -0,0 +1,11 @@
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+    
+    while ((ch = getchar()) != '\n')
+        if(i < n)
+            str[i++] = ch;
+    str[i] = '\0';                          /*terminates string */
+    return i;                               /*number of characters stored */
+}
