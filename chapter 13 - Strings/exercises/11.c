cd009d7132f59ed15aee7e9bdbc1c5b5d55f3138
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-28 16:08:38 +0100

    Create 11.c

---
+++ chapter 13 - Strings/exercises/11.c
@@ -0,0 +1,29 @@
+/**********************************************************************************
+The Q&A section at the end of this chapter shows how the strcmp function
+might be written using array subscripting. Modify the function to use pointer
+arithmetic instead.
+***********************************************************************************/
+
+//array subscripting
+int strcmp(char *s, char *t)
+{
+    int i;
+    char *s, *t;
+
+    for(i = 0; s[i] == t[i]; i++)
+        if(s[i] == '\0')
+            return 0;
+    return s[i] - t[i];
+
+
+
+}
+
+//pointer arithmetic
+int strcmp(char *s, char *t)
+{
+    while(*s++ == *t++)
+        if(!*s)
+            return 0;
+    return *(--s) - *(--t);
+}
