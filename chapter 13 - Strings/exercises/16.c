5feb45cc460162bca2b50d97eda1744be8354a37
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-10-29 10:45:59 +0100

    Create 16.c

---
+++ chapter 13 - Strings/exercises/16.c
@@ -0,0 +1,27 @@
+/**********************************************************************************
+Use the techniques of Section 13.6 to condense the count_spaces function
+of Section 13.4. In particular, replace the for statement by a while loop.
+***********************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int count_spaces(const char *s);
+
+int main(void)
+{
+    const char string[] = "Just a test sentence to try out the function.";
+
+    printf("%d", count_spaces(string));
+}
+
+int count_spaces(const char *s)
+{
+    int count = 0;
+
+    while(*s)
+        if(*s++ == ' ')
+            count++;
+    
+    return count;
+}
