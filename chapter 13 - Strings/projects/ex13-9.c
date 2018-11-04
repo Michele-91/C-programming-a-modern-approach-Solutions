b525f7d9e2294ab3b89aa4ceb0edad597e8a8699
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-04 17:40:42 +0100

    Add files via upload

---
+++ chapter 13 - Strings/projects/ex13-9.c
@@ -0,0 +1,54 @@
+/**********************************************************************************
+Modify Programming Project 10 from Chapter 7 so that it includes
+the following function:
+
+int computer_vowel_count(const char *sentence);
+
+The function returns the number of vowels in the string pointed to 
+by the sentence parameter
+***********************************************************************************/
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define LEN 40
+
+int computer_vowel_count(const char *sentence);
+
+int main()
+{
+
+    char ch[LEN + 1];
+
+    printf("Enter a sentence: ");
+    gets(ch);
+
+    printf("Your sentence contains %d vowels\n", computer_vowel_count(ch));
+    
+    return 0;
+}
+
+int computer_vowel_count(const char *sentence)
+{
+    int vowels = 0;
+    
+    while(*sentence)
+    {
+        switch(toupper(*sentence++))
+        {
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+            vowels++; break;
+            default: break;
+        }
+    }
+
+    return vowels;
+}
+
+
+
+
+
+
+
