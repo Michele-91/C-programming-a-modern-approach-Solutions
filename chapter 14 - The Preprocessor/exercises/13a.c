cd22bef76f0db20a13dc3490bb31ffc4777e0423
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-20 19:01:01 +0100

    Create 13a.c

---
+++ chapter 14 - The Preprocessor/exercises/13a.c
@@ -0,0 +1,23 @@
+Lines brought in by stdio.h
+blank line
+blank line
+blank line
+void f(void);
+blank line
+int main(void)
+{
+    f();
+blank line
+blank line
+blank line
+    return 0;
+}
+
+void f(void)
+{
+    blank line
+        printf("N is %d\n, 100)
+    blank line
+        printf("N is undefined\n");
+    blank line
+}
